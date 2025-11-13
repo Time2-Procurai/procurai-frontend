15ec9e34b64179cc7d3cc6fbd8096d4a07c5bd38
Gabrielgsw <ggermanow279@gmail.com>
2025-11-13 11:20:23 -0300

    inserindo url base de produção

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  //baseURL: "http://127.0.0.1:8000/api/",    // url local, se for testar localmente, comentar linha abaixo e descomentar essa
+  baseURL: "http://137.184.151.61:8080/api/", // url produção
 });
 
 
